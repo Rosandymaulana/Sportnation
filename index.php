e507952f0596cbe54dcad29bc9eb62bb123b96a6
Rosandymaulana <rosandy1118@gmail.com>
2022-04-28 13:00:25 +0700

    parts

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <?php
 session_start();
+include "php/connect.php";
 ?>
 
 <head>
@@ -20,49 +21,9 @@ session_start();
 </head>
 
 <body>
-    <nav>
-        <div class="nav-bar">
-            <i class='bx bx-menu sidebarOpen'></i>
-
-            <span class="logo navLogo">
-                <a href="#">
-                    <img class="brand" src="images/logo.png" alt="" style="width: 1.5rem; margin-right: .7rem;">
-                    Sportnation
-                </a>
-            </span>
-
-            <div class="menu">
-                <div class="logo-toggle">
-                    <span class="logo"><a href="index.php">Sportnation</a></span>
-                    <i class='bx bx-x siderbarClose'></i>
-                </div>
-
-                <ul class="nav-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#sportvenue">SportVenue</a></li>
-                    <li><a href="#rooms">Rooms</a></li>
-                    <li><a href="#content">Content</a></li>
-                    <li><a href="Pages/Customer Service/index.html">Support</a></li>
-                    <?php
-                    include "php/connect.php";
-                    if (isset($_SESSION['status'])) {
-                    ?>
-                        <li><a href="Pages/Account/index.php">Account</a></li>
-                </ul>
-            </div>
-            <a href="php/logout.php" class="register" style="background-color:#a9e7cc; color: #1D1D1D;">Logout</a>
-        <?php
-                    } else {
-        ?>
-            <div class="button-box">
-                <a href="Pages/Login/index.html" class="login" style="background-color:#a9e7cc; color: #1D1D1D;">Masuk</a>
-                <a href="Pages/Register/index.html" class="register" style="background-color:#008080">Daftar</a>
-            <?php
-                    }
-            ?>
-            </div>
-        </div>
-    </nav>
+    <?php
+    include "Pages/parts/header/index.php";
+    ?>
     <main>
         <div class="hero-section" id="home">
             <div class="left">
