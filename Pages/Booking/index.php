b290a1a559ab3d0b38732f1b82b0edf5321158e2
Rosandymaulana <rosandy1118@gmail.com>
2022-04-29 16:39:03 +0700

    footer

--- Pages/Booking/index.php
+++ Pages/Booking/index.php
@@ -5,6 +5,7 @@ session_start();
 include "../../php/connect.php";
 $id = $_SESSION['id'];
 ?>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -26,31 +27,31 @@ $id = $_SESSION['id'];
                 <h3>Reservation</h3>
                 <form action="../../php/booking.php" method="get">
                     <?php
-        $query = "Select * from user where user_id = '$id'";
-        $result = mysqli_query($connect, $query);
-        while ($row = mysqli_fetch_array($result)) {
-        ?>
-                    <div class="form-row">
-                        <input name="date" type="date" placeholder="Choose Date" required>
-                        <input name="time" type="time" placeholder="Set Time" min="09:00" max="18:00" required>
-                    </div>
+                    $query = "Select * from user where user_id = '$id'";
+                    $result = mysqli_query($connect, $query);
+                    while ($row = mysqli_fetch_array($result)) {
+                    ?>
+                        <div class="form-row">
+                            <input name="date" type="date" placeholder="Choose Date" required>
+                            <input name="time" type="time" placeholder="Set Time" min="09:00" max="18:00" required>
+                        </div>
 
-                    <div class="form-row">
-                        <input name="name" value="<?php echo $row['nama'] ?>" type="text" placeholder="Full Name">
-                        <input name="phone" value="<?php echo $row['no_telp'] ?>" type="text" placeholder="Phone Number">
-                    </div>
+                        <div class="form-row">
+                            <input name="name" value="<?php echo $row['nama'] ?>" type="text" placeholder="Full Name">
+                            <input name="phone" value="<?php echo $row['no_telp'] ?>" type="text" placeholder="Phone Number">
+                        </div>
 
-                    <div class="form-row">
-                        <input name="jam" type="number" placeholder="Time playing? (in hours)" min="1">
+                        <div class="form-row">
+                            <input name="jam" type="number" placeholder="Time playing? (in hours)" min="1">
 
-                    </div>
-                    <div class="form-row center">
-                        <input type="submit" value="Book Now">
-                    </div>
-                    
-        <?php
-        }
-        ?>
+                        </div>
+                        <div class="form-row center">
+                            <input type="submit" value="Book Now">
+                        </div>
+
+                    <?php
+                    }
+                    ?>
             </div>
         </div>
     </section>
