dab82372155cd587ba99b549356fb57fdf243f70
Rosandymaulana <rosandy1118@gmail.com>
2022-06-02 10:29:10 +0700

    booking

--- php/booking.php
+++ php/booking.php
@@ -7,17 +7,18 @@ $jam = $_GET['jam'];
 $date = $_GET['date'];
 $time = $_GET['time'];
 $lapangan = $_GET['lapangan'];
+$lapangan = explode(" ", $lapangan);
+$total = $jam * $lapangan[0];
 
 //Menghitung total
-$sql = "SELECT harga FROM lapangan where lap_id = '$lapangan'";
+$sql = "SELECT harga FROM lapangan where lap_id = '$lapangan[1]'";
 $result = mysqli_query($connect, $sql);
 $row = mysqli_fetch_array($result);
-$total = $row['harga'] * $jam;
 
 $query = "SELECT nomor FROM booking ORDER BY nomor DESC LIMIT 1";
 $nomor = mysqli_query($connect, $query);
 $nomor = $nomor->fetch_array();
-if($nomor > 0) {
+if ($nomor > 0) {
     $nomorInt = intval($nomor[0]) + 1;
 } else {
     $nomorInt = 1;
@@ -25,12 +26,12 @@ if($nomor > 0) {
 
 $query = "INSERT INTO booking (book_id, nomor, user_id, lap_id, durasi, total, tgl, jam)
             VALUES
-                ('B-0$nomorInt', '$nomorInt', '$user_id', '$lapangan', '$jam', '$total', '$date', '$time')
+                ('B-0$nomorInt', '$nomorInt', '$user_id', '$lapangan[1]', '$jam', '$total', '$date', '$time')
          ";
 
-         mysqli_query($connect, $query);
-         
-echo "<script>window.location.href='../Pages/Pembayaran/index.php?uid=$nomorInt'</script>"
+mysqli_query($connect, $query);
+
+echo "<script>window.location.href='../Pages/Pembayaran/index.php?uid=$nomorInt'</script>";
 
     // if(mysqli_query($connect, $query)){
     //     echo "<script> 
@@ -43,4 +44,3 @@ echo "<script>window.location.href='../Pages/Pembayaran/index.php?uid=$nomorInt'
     //         document.location.href = '../Pages/Booking/index.php';
     //     </script> <br>";
     // }
-?>
