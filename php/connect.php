676c6ee53af9c001ba6cf4d6785f29995435a632
Moch Fajrul Falah <mfajrulfalah.ff@gmail.com>
2022-06-15 00:34:15 +0700

    connect

--- php/connect.php
+++ php/connect.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
-$database = "sportnation";
-$username = "root";
-$password = "";
+$hostname = "localhost";
+$database = "id18109489_sportnation";
+$username = "id18109489_sport";
+$password = "J?nkA~nlh\^1K|\x";
 // Create connection
-$connect = mysqli_connect($servername, $username, $password, $database);
+$connect = mysqli_connect($hostname, $username, $password, $database);
 // Check connection
 if (!$connect) {
     die("Connection failed: " . mysqli_connect_error());
